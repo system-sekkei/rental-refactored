fe862e62c91b6559facfeb286991164776ad9fe8
masuda220 <masuda220@gmail.com>
2017-10-13 14:22:31 +0900

    金額を整数に（ドルから円に）

--- src/main/java/rental/Customer.java
+++ src/main/java/rental/Customer.java
@@ -20,24 +20,24 @@ class Customer {
     }
 
     String statement() {
-        double totalAmount = 0;
+        int totalAmount = 0;
         int frequentRenterPoints = 0;
 
         String result = "Rental Record for " + name() + "\n";
 
         for (Rental each : rentals) {
-            double thisAmount = 0;
+            int thisAmount = 0;
             switch( each.movie().priceCode()) {
                 case Movie.REGULAR:
-                    thisAmount += 2;
-                    if( each.daysRented()>2) thisAmount += (each.daysRented -2 ) * 1.5;
+                    thisAmount += 200;
+                    if( each.daysRented()>2) thisAmount += (each.daysRented -2 ) * 150;
                     break;
                 case Movie.NEW_RELEASE:
-                    thisAmount += each.daysRented() * 3;
+                    thisAmount += each.daysRented() * 300;
                     break;
                 case Movie.CHILDREN:
-                    thisAmount += 1.5;
-                    if(each.daysRented() > 3) thisAmount += (each.daysRented() -3 ) * 1.5;
+                    thisAmount += 150;
+                    if(each.daysRented() > 3) thisAmount += (each.daysRented() -3 ) * 150;
                     break;
             }
 
@@ -45,14 +45,14 @@ class Customer {
             if( each.movie().priceCode() == Movie.NEW_RELEASE && each.daysRented() > 1 )
                 frequentRenterPoints ++;
 
-            result += "\t" + each.movie().title() + "\t" + String.valueOf(thisAmount) + "\n";
+            result += "\t" + each.movie().title() + "\t" + thisAmount + "\n";
 
             totalAmount += thisAmount;
 
         }
 
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+        result += "Amount owed is " + totalAmount + "\n";
+        result += "You earned " + frequentRenterPoints + " frequent renter points";
         return result;
     }
 }
--- src/test/java/rental/CustomerTest.java
+++ src/test/java/rental/CustomerTest.java
@@ -6,10 +6,15 @@ import static org.junit.Assert.*;
 
 public class CustomerTest {
 
+    static final Movie newOne = new Movie("新作", Movie.NEW_RELEASE);
+    static final Movie children = new Movie("子供向け", Movie.CHILDREN);
+    static final Movie regular = new Movie("一般", Movie.REGULAR);
     @Test
     public void smokeTest() {
         Customer customer = new Customer("増田") ;
-        customer.addRental(new Rental(new Movie("title", Movie.NEW_RELEASE), 3));
+        customer.addRental(new Rental(newOne,3));
+        customer.addRental(new Rental(children,2));
+        customer.addRental(new Rental(regular,1));
         System.out.println(customer.statement());
     }
 
