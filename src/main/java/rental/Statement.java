f6b8f6d7a8e87cbff680c1934aca5b5b62f9a659
masuda220 <masuda220@gmail.com>
2017-10-27 16:05:42 +0900

    金額計算の switch文を Rental に移動 #11

--- src/main/java/rental/Statement.java
+++ src/main/java/rental/Statement.java
@@ -16,20 +16,7 @@ class Statement {
         String result = "レンタル記録 " + customer.name() + "\n";
 
         for (Rental each : customer.rentals.asList()) {
-            int thisAmount = 0;
-            switch( each.movie().movieType()) {
-                case REGULAR:
-                    thisAmount += 200;
-                    if( each.daysRented()>2) thisAmount += (each.daysRented -2 ) * 150;
-                    break;
-                case NEW_RELEASE:
-                    thisAmount += each.daysRented() * 300;
-                    break;
-                case CHILDREN:
-                    thisAmount += 150;
-                    if(each.daysRented() > 3) thisAmount += (each.daysRented() -3 ) * 150;
-                    break;
-            }
+            int thisAmount = each.amount();
 
             frequentRenterPoints ++;
             if( each.movie().movieType() == NEW_RELEASE && each.daysRented() > 1 )
@@ -38,7 +25,6 @@ class Statement {
             result += "\t" + each.movie().title() + "\t" + thisAmount + "\n";
 
             totalAmount += thisAmount;
-
         }
 
         result += "レンタル金額 " + totalAmount + "円\n";
