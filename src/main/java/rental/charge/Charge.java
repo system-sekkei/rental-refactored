80aa21b6ff6d2b027a8fa93bc9965175d5fc112d
masuda220 <masuda220@gmail.com>
2018-03-09 17:41:10 +0900

    料金計算の未使用クラス群を削除、Chargeクラスの導入

--- src/main/java/rental/charge/Charge.java
+++ src/main/java/rental/charge/Charge.java
@@ -1,7 +1,10 @@
 package rental.charge;
 
-import rental.DaysRented;
+//TODO ：料金計算を int から　この値オブジェクトに変更する
+public class Charge {
+    int value ;
 
-public interface Charge {
-    public int amountFor(DaysRented daysrented);
+    public Charge(int value) {
+        this.value = value;
+    }
 }
